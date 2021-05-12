f7920520d5a9b1b8ef19b8125c9b4c96fb9c9dc7
karthiksripad18 <karthiksripad18@gmail.com>
2021-05-12 20:53:25 +0530

    added Rochester font family to the  project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "rochester": ["Rochester"]
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
