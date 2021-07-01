b1fb9828d83acf8f08b9f844872705fc08fa2027
karthiksripad18 <karthiksripad18@gmail.com>
2021-07-01 23:19:15 +0530

    added badge to cart icon in navbar

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Link, useLocation} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faShoppingCart, faUser} from '@fortawesome/free-solid-svg-icons';
+import {selectCartItems} from '../redux/cartSlice';
+import {useSelector} from 'react-redux';
 
 interface Props {
     isOpen: boolean,
@@ -10,6 +12,7 @@ interface Props {
 
 const Dropdown : React.FC<Props>= ({isOpen, toggle}) => {
     const {pathname} = useLocation();
+    const { items }: any = useSelector(selectCartItems);
     return (
         <div onClick={() => toggle()} className={isOpen? "grid grid-rows-4 w-100 text-center text-2xl": 'hidden'}>
                 <ul>
@@ -23,9 +26,10 @@ const Dropdown : React.FC<Props>= ({isOpen, toggle}) => {
                             Products
                         </Link>
                     </li>
-                    <li className={pathname === '/cart' ? "bg-white text-primary px-2 hover-effect shadowed-2xl border-2 rounded-full": "p-1 hover-effect"}>
+                    <li className={pathname === '/cart' ? "bg-white text-primary px-2 hover-effect shadowed-2xl border-2 rounded-full relative": "p-1 hover-effect relative"}>
                         <Link to="/cart">
                             <FontAwesomeIcon icon={faShoppingCart} color={pathname === '/cart'? "#2f303a" :"white"} />
+                            {items.length > 0 ? <div className="px-1 font-sans text-xs font-bold rounded-full bg-primaryButton" style={{position: "absolute", top: 0, left: "50%"}}>{items.length}</div>: null}
                         </Link>
                     </li>
                     <li className={pathname === '/user' ? "bg-white text-primary px-2 hover-effect shadowed-2xl border-2 rounded-full": "p-1 hover-effect"}>
