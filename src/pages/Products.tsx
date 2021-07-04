1f709678ca054375440745c3627acda72942f9b3
karthiksripad18 <karthiksripad18@gmail.com>
2021-07-04 12:13:59 +0530

    added environment variables and env-cmd package

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {useDispatch} from 'react-redux';
 import {setLoading} from '../redux/loadingSlice';
-import {FAKEAPIURL, ALL,MENS, WOMENS, JEWELLERY, ELECTRONICS } from '../common/constants';
+import {ALL,MENS, WOMENS, JEWELLERY, ELECTRONICS } from '../common/constants';
 import Product from '../components/Product';
 
 const Products: React.FC = () => {
@@ -13,7 +13,7 @@ const Products: React.FC = () => {
     useEffect(() => {
         dispatch(setLoading({isLoading: true, loadingMessage: "Fetching your Products"}));
         const getProducts = () => {
-            const url = selectedCategory? `${FAKEAPIURL}products/category/${selectedCategory}`: `${FAKEAPIURL}products`;
+            const url = selectedCategory? `${process.env.REACT_APP_API_URL}products/category/${selectedCategory}`: `${process.env.REACT_APP_API_URL}products`;
             axios.get(url)
             .then(
                 ({data}) => {
@@ -26,15 +26,15 @@ const Products: React.FC = () => {
     }, [selectedCategory, dispatch]);
 
     return (
-        <div className="w-full h-full flex flex-col">
-            <div className="w-full mb-5 flex flex-col items-center sm:flex-row">
+        <div className="flex flex-col w-full h-full">
+            <div className="flex flex-col items-center w-full mb-5 sm:flex-row">
                 <div onClick={() => SetSelectedCategory(ALL)} className={selectedCategory === ALL? "underline category": "category"}>All</div>
                 <div onClick={() => SetSelectedCategory(MENS)} className={selectedCategory === MENS? "underline category": "category"}>Men's</div>
                 <div onClick={() => SetSelectedCategory(WOMENS)} className={selectedCategory === WOMENS? "underline category": "category"}>Women's</div>
                 <div onClick={() => SetSelectedCategory(JEWELLERY)} className={selectedCategory === JEWELLERY? "underline category": "category"}>Jewellery</div>
                 <div onClick={() => SetSelectedCategory(ELECTRONICS)} className={selectedCategory === ELECTRONICS? "underline category": "category"}>Electronics</div>
             </div>
-            <div className="h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-5">
+            <div className="grid h-full grid-cols-1 gap-4 mx-5 md:grid-cols-2 lg:grid-cols-3">
                 {products.length > 0?
                     products.map((product,i) => <Product {...product} key={i} />)
                     : null
